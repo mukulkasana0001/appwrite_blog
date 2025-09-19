dad619996e16a50a1f37582af2e4ce789122078b
mukul kasana <mukulkasana0001@gmail.com>
2025-09-19 10:07:30 +0530

    api key update

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,54 +1,99 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+// import React from 'react'
+// import {Editor } from '@tinymce/tinymce-react';
+// import {Controller } from 'react-hook-form';
 
 
-export default function RTE({name, control, label, defaultValue =""}) {
+// export default function RTE({name, control, label, defaultValue =""}) {
+//   return (
+//     <div className='w-full'> 
+//     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+//     <Controller
+//     name={name || "content"}
+//     control={control}
+//     render={({field: {onChange}}) => (
+//         <Editor
+//         initialValue={defaultValue}
+//         init={{
+//             initialValue: defaultValue,
+//             height: 500,
+//             menubar: true,
+//             plugins: [
+//                 "image",
+//                 "advlist",
+//                 "autolink",
+//                 "lists",
+//                 "link",
+//                 "image",
+//                 "charmap",
+//                 "preview",
+//                 "anchor",
+//                 "searchreplace",
+//                 "visualblocks",
+//                 "code",
+//                 "fullscreen",
+//                 "insertdatetime",
+//                 "media",
+//                 "table",
+//                 "code",
+//                 "help",
+//                 "wordcount",
+//                 "anchor",
+//             ],
+//             toolbar:
+//             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+//             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+//         }}
+//         onEditorChange={onChange}
+//         />
+//     )}
+//     />
+
+//      </div>
+//   )
+// }
+
+
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1" htmlFor={name}>
+          {label}
+        </label>
+      )}
 
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
-
-     </div>
-  )
+      <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { onChange, value } }) => (
+          <Editor
+            apiKey="4t03o9v3p4g5izcf6jqrjl6o76pcqhqayi65uu9smh6rhfh5" // replace with your TinyMCE API key in production
+            value={value}
+            onEditorChange={onChange}
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
+                "advlist autolink lists link image charmap preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table help wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | bold italic forecolor | " +
+                "alignleft aligncenter alignright alignjustify | " +
+                "bullist numlist outdent indent | removeformat | help | image",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+          />
+        )}
+      />
+    </div>
+  );
 }
